4edc1da2c3e0709766162019f945c46aac243dfe
Hau Le 02 <haulp0210@gmail.com>
2024-11-20 22:16:14 +0700

    Cai dat du an

---
+++ src/components/FooterStore.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="background-color: #2c2d6e; color: white; padding: 20px 10px; font-family: Arial, sans-serif;">
+        <div class="container d-flex flex-wrap">
+            <!-- Phần thông tin logo và mô tả -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="left_info d-flex flex-column col-lg-5" style="margin-left:61px;">
+                    <img src="../assets/img/logo2.png" class="img-fluid" alt="..." width="150" height="150" style="    margin-bottom: 0px;
+                    margin-left: 60px;">
+                </div>
+                <p style="margin: 10px 0 0 50px;">
+                    Hãy hợp lý hóa thói quen thêm ngày thứ tám vào tuần để dành riêng cho việc đọc sách
+                </p>
+            </div>
+
+            <!-- Phần "Về chúng tôi" -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <h5 style="font-weight: bold;">Về chúng tôi </h5>
+                <p><i class="fas fa-map-marker-alt"></i> 123 Xuân Khánh, Ninh Kiều, Cần Thơ</p>
+                <p><i class="fas fa-envelope"></i> info@gmail.com</p>
+                <p><i class="fas fa-phone"></i> 02933 003 754</p>
+            </div>
+
+            <!-- Phần "Liên kết nhanh" -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <h5 style="font-weight: bold;">Liên kết nhanh</h5>
+                <ul style="list-style: none; padding: 0; margin: 0;">
+                    <li><a href="#" style="color: white; text-decoration: none;">Trang chủ</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">Giới thiệu</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">Liên hệ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/Introduce.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container" id="main_page">
+        <div class="container" id="main_content">
+            <h3>HOẠT ĐỘNG KINH DOANH</h3>
+            <hr>
+            <div class="row">
+                <p>
+                    <span>- Về văn hóa:</span> Sách quốc văn với nhiều thể loại đa dạng như sách giáo khoa – tham khảo,
+                    giáo trình, sách học ngữ, từ điển, sách tham khảo thuộc nhiều chuyên ngành phong phú: văn học, tâm
+                    lý – giáo dục, khoa học kỹ thuật, khoa học kinh tế - xã hội, khoa học thường thức, sách phong thủy,
+                    nghệ thuật sống, danh ngôn, sách thiếu nhi, truyện tranh, truyện đọc, từ điển, công nghệ thông tin,
+                    khoa học – kỹ thuật, nấu ăn, làm đẹp... của nhiều Nhà xuất bản, nhà cung cấp sách có uy tín như: NXB
+                    Trẻ, Giáo Dục, Kim Đồng, Văn hóa -Văn Nghệ, Tổng hợp TP.HCM, Chính Trị Quốc Gia; Công ty Đông A, Nhã
+                    Nam, Bách Việt, Alphabook, Thái Hà, Minh Lâm, Đinh Tị, Minh Long, TGM, Sáng Tạo Trí Việt, Khang
+                    Việt, Toàn Phúc…
+                    <br>
+                    <br>
+                    <span> - Tầm nhìn: </span>
+                    Quyết tâm trở thành một thương hiệu quốc gia, uy tín trong nước và hội nhập quốc tế lấy “giáo dục,
+                    phát triển tương lai” làm trung tâm với mạng lưới các sản phẩm, dịch vụ: sách và thư viện - không
+                    gian đọc, khu
+                    vui chơi thiếu nhi, thiết bị giáo dục, văn phòng phẩm.
+                    nghiệp.
+                    <br>
+                    <br>
+                    <span> - Sứ mệnh:</span>
+                    Cam kết không ngừng tạo ra và thúc đẩy tiêu dùng những dịch vụ và sản phẩm văn hóa có giá trị cao,
+                    góp phần gây dựng và nuôi dưỡng một tương lai tốt đẹp.
+                    <br>
+                    <span> - Giá trị cốt lõi:</span>
+                    Sẽ không ngừng gia tăng sự đóng góp tài lực - vật lực cho cộng đồng, cho xã hội nhằm xây dựng hình
+                    ảnh một doanh nghiệp phát triển bền vững và nhân văn - cam kết trở thành địa chỉ tin cậy cho khách
+                    hàng.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+#main_page {
+    height: 100%;
+}
+
+#main_content {
+    border: 2px solid #62ab00;
+    margin: 50px;
+    padding: 50px;
+    border-radius: 10px;
+}
+
+span {
+    color: #62ab00;
+    font-weight: bold;
+}
+</style>
